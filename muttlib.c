4262a578047550e2d93e39a6d0931d8ae371f9d3
Aaron Schrab <aaron@schrab.com>
2011-12-06 15:16:55 -0500

    Apply David Champion's deepif patch
    From http://home.uchicago.edu/~dgc/sw/mutt/patch-1.5.1.dgc.deepif.1

--- muttlib.c
+++ muttlib.c
@@ -1275,6 +1275,12 @@ void mutt_FormatString (char *dest,		/* output buffer */
 	count = 0;
         while (count < sizeof (ifstring) && *src && *src != '?' && *src != '&')
 	{
+	  if (*src == '\\')
+	  {
+	    src++;
+	    if (!*src)
+	      break;
+	  }
           *cp++ = *src++;
 	  count++;
 	}
@@ -1287,7 +1293,13 @@ void mutt_FormatString (char *dest,		/* output buffer */
 	count = 0;
 	while (count < sizeof (elsestring) && *src && *src != '?')
 	{
-	  *cp++ = *src++;
+	  if (*src == '\\')
+	  {
+	    src++;
+	    if (!*src)
+	      break;
+	  }
+          *cp++ = *src++;
 	  count++;
 	}
 	*cp = 0;
