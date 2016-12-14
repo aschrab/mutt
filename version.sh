888fbffe3e733ae5d3981788a893ca7fdb06e272
Aaron Schrab <aaron@schrab.com>
2016-12-14 01:20:31 -0500

    Don't use `git describe` to get version history
    The description generated by `git describe` often won't accurately
    reflect the most recent version of mutt that is included due to how tags
    get added. Work around this by getting the highest-numbered tag that is
    included, and work out how many commits are not included on that but are
    in the working tree.

--- version.sh
+++ version.sh
@@ -68,12 +68,16 @@ fi
 
 # If git is present and in a git repo use that to build detailed version string
 if git rev-parse 2> /dev/null; then
-	# Transform string like "mutt-1-6-1-rel-108-g2415feb+" into "1.6.1+108 (g2415feb+)"
-	git describe --long --tags --match 'mutt-*-rel' --dirty=+ | sed \
-		-e 's/mutt-//' \
-		-e 's/-rel-\([0-9]*\)/+\1/' \
-		-e 's/-\(g.*\)/ (\1)/' \
-		-e 's/-/./g'
+	latesttag="$(git tag --merged=HEAD --list 'mutt-*-rel' | tr - . | sort -Vr | head -n1 | tr . -)"
+	version="$(echo $latesttag | sed -e s/mutt-// -e s/-rel// -e s/-/./g)"
+	distance="$(git rev-list --count $latesttag..)"
+	commitid="$(git rev-parse --short HEAD)"
+	if [ -n "$(git status --porcelain --untracked-files=no)" ]; then
+	  dirty=+
+	else
+	  dirty=""
+	fi
+	echo "${version}+${distance} (g${commitid}${dirty})"
 	exit 0
 fi
 
